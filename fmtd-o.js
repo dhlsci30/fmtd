29e4d7d80551440bae25c6eb5a243fb12a6f7e65
dhlsci30 <steven.martin@dhl.com>
2026-07-06 09:08:35 +1000

    Update fmtd-o.js

--- fmtd-o.js
+++ fmtd-o.js
@@ -102,7 +102,7 @@ async function searchOrder(orderId, email) {
 }
 
 async function c(orderId) {
-    if (abc.substring(0,3) == "DSC") {
+    if (orderId.substring(0,3) == "DSC") {
         domain = "PLNAU";
     } else {
         domain = "AUNWP";
